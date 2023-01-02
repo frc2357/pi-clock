4a4040d8b2e7f812855146c486937a04f3ba1465
coderkevin <kevin@redfenix.com>
2023-01-02 11:06:01 -0600

    Add built version of react app to the node balena app
    This adds a built version of the react app to be served statically
    from the node express server.

--- nodejs-react-pi-clock/src/index.js
+++ nodejs-react-pi-clock/src/index.js
@@ -4,8 +4,6 @@ const PORT = 80;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Test from express");
-});
+app.use("/", express.static("react-app/build"));
 
 app.listen(PORT, () => console.log(`Express server listening on port ${PORT}`));
