dba51490b7fc17fd6452fa7224838a7e7604d72d
coderkevin <kevin@redfenix.com>
2023-01-29 17:48:57 -0600

    Updates

---
+++ timeclock-config.js
@@ -0,0 +1,6 @@
+window.timeclockConfig = {
+  apiKey: "AIzaSyD0P_i6gxfRMAak7hlEVCaeMlVPXNeVtyE",
+  clientId:
+    "567997354242-vsa94of2g76p46ccc6lsicj87eqfc900.apps.googleusercontent.com",
+  spreadsheetId: "1JNK59VvA-3CsU5eMQIKKIKH1X69JVBHnH50tFlZyaqk",
+};
