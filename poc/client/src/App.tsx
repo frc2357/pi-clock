e097320b08074fb55268303aba8b3c4af95dae34
Tyson453 <tyson112305@gmail.com>
2025-05-08 15:33:39 -0500

    Sign in with google firebase

--- poc/client/src/App.tsx
+++ poc/client/src/App.tsx
@@ -2,9 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import GoogleLoginButton from './components/GoogleLoginButton'
+import { type User } from 'firebase/auth'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const onLoginSuccess = (user: User) => {
+    console.log(user);
+  }
+
+  const onLoginError = (error: unknown) => {
+    console.log(error);
+  }
 
   return (
     <>
@@ -28,6 +38,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <GoogleLoginButton onLogin={onLoginSuccess} onError={onLoginError} />
     </>
   )
 }
