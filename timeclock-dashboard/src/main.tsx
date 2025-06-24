3d9137ce8338f31830e126dc2d58e1f10a8ff4d6
Tyson453 <tyson112305@gmail.com>
2025-06-24 17:51:44 -0500

    Layout

--- timeclock-dashboard/src/main.tsx
+++ timeclock-dashboard/src/main.tsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexReactClient } from "convex/react";
+import {
+  createTheme,
+  CssBaseline,
+  type ThemeOptions,
+  ThemeProvider,
+} from "@mui/material";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#121212",
+      paper: "#1e1e1e",
+    },
+    primary: {
+      main: "#ff8811",
+    },
+    secondary: {
+      main: "#47FF44",
+    },
+  },
+};
+
+const theme = createTheme(themeOptions);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <ConvexAuthProvider client={convex}>
+        <CssBaseline />
+        <App />
+      </ConvexAuthProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
