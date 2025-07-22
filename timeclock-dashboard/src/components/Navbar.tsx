f18951e52187d1a0226adaab3987a9ea6ba130a5
Tyson453 <tyson112305@gmail.com>
2025-07-21 20:41:09 -0500

    team member table

--- timeclock-dashboard/src/components/Navbar.tsx
+++ timeclock-dashboard/src/components/Navbar.tsx
@@ -3,17 +3,23 @@ import logo from "../assets/logo.png";
 import { Authenticated } from "convex/react";
 
 export default function Navbar() {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <img style={{ height: 40, width: 40 }} src={logo} />
-        <Typography variant="h5" sx={{ margin: 2 }}>
-          FRC2357 Timeclock
-        </Typography>
-        <Authenticated>
-          <Button href="/logout">Logout</Button>
-        </Authenticated>
-      </Toolbar>
-    </AppBar>
-  );
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <img style={{ height: 40, width: 40 }} src={logo} />
+                <Typography variant="h5" sx={{ margin: 2 }}>
+                    FRC2357 Timeclock
+                </Typography>
+                <Authenticated>
+                    <Button
+                        variant="outlined"
+                        sx={{ marginLeft: "auto" }}
+                        href="/logout"
+                    >
+                        Logout
+                    </Button>
+                </Authenticated>
+            </Toolbar>
+        </AppBar>
+    );
 }
--- timeclock-dashboard/src/pages/HomePage.tsx
+++ timeclock-dashboard/src/pages/HomePage.tsx
@@ -1,3 +1,85 @@
+import {
+    Box,
+    Chip,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+} from "@mui/material";
+import { useQuery } from "convex/react";
+import { useEffect } from "react";
+import { api } from "../../convex/_generated/api";
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+    const teamMembers = useQuery(api.team_member.list);
+
+    useEffect(() => {
+        console.log(teamMembers);
+    }, [teamMembers]);
+
+    return (
+        <Box sx={{ height: "100%" }}>
+            <Typography variant="h3" sx={{ margin: 5 }}>
+                Team Dashboard
+            </Typography>
+            <Paper sx={{ width: "auto", marginX: 10 }}>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={{ width: "40%" }}>
+                                    Team Member
+                                </TableCell>
+                                <TableCell align="right" sx={{ width: "20%" }}>
+                                    Total Hours
+                                </TableCell>
+                                <TableCell align="right" sx={{ width: "20%" }}>
+                                    Last Clock In
+                                </TableCell>
+                                <TableCell align="right" sx={{ width: "20%" }}>
+                                    Status
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {teamMembers?.map((member) => (
+                                <TableRow key={member._id}>
+                                    <TableCell>{member.user?.name}</TableCell>
+                                    <TableCell align="right">
+                                        {member.total_hours}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        {member.latest_event?.clock_in
+                                            ? new Date(
+                                                  member.latest_event.clock_in
+                                              ).toLocaleString()
+                                            : "Never"}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <Chip
+                                            sx={{ width: 150 }}
+                                            label={
+                                                member.active
+                                                    ? "Active"
+                                                    : "Inactive"
+                                            }
+                                            color={
+                                                member.active
+                                                    ? "success"
+                                                    : undefined
+                                            }
+                                        />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </Box>
+    );
 }
